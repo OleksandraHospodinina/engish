6bfecaab012ed01ff72e314c8228172fbe2e3dd9
OleksandraHospodinina <166499795+OleksandraHospodinina@users.noreply.github.com>
2024-06-27 23:21:37 +0000

    created js for format

--- js/formats.js
+++ js/formats.js
@@ -7,12 +7,19 @@ const adultsSection = document.querySelector(".format__container.adults");
 kidsTitle.addEventListener("click", function () {
   kidsSection.style.display = "block";
   adultsSection.style.display = "none";
+  
+  kidsTitle.classList.add("active-title");
+  adultsTitle.classList.remove("active-title");
 });
 
 adultsTitle.addEventListener("click", function () {
   kidsSection.style.display = "none";
   adultsSection.style.display = "block";
+  
+  adultsTitle.classList.add("active-title");
+  kidsTitle.classList.remove("active-title");
 });
 
 kidsSection.style.display = "block";
 adultsSection.style.display = "none";
+kidsTitle.classList.add("active-title");
