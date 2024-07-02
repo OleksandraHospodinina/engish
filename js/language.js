865881c452b59b7135a6a06528112ce063ea76c9
OleksandraHospodinina <166499795+OleksandraHospodinina@users.noreply.github.com>
2024-07-02 13:13:52 +0000

    created translation

---
+++ js/language.js
@@ -0,0 +1,8 @@
+function setLanguage(lang) {
+    if (lang === 'ua') {
+      window.location.href = 'index.html';
+    } else if (lang === 'eng') {
+      window.location.href = 'index-eng.html';
+    }
+  }
+  
