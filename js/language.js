7cae5804e71682e1e53f21b57463f9f719c25ea9
OleksandraHospodinina <166499795+OleksandraHospodinina@users.noreply.github.com>
2024-07-02 13:19:16 +0000

    upd styles

--- js/language.js
+++ js/language.js
@@ -1,8 +1,7 @@
 function setLanguage(lang) {
-    if (lang === 'ua') {
-      window.location.href = 'index.html';
-    } else if (lang === 'eng') {
-      window.location.href = 'index-eng.html';
-    }
+  if (lang === "ua") {
+    window.location.href = "index.html";
+  } else if (lang === "eng") {
+    window.location.href = "index-eng.html";
   }
-  
+}
